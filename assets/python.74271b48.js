4e2f6a7f70d1fee5ad69b8114e1c49ac743a587e
潘昱辰 <2718911600@qq.com>
2022-11-23 20:03:07 +0800

    fix

--- assets/python.74271b48.js
+++ assets/python.74271b48.js
@@ -1,4 +1,4 @@
-import{m as a}from"./index.dd81670f.js";/*!-----------------------------------------------------------------------------
+import{m as a}from"./index.619968b5.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.34.1(547870b6881302c5b4ff32173c16d06009e3588f)
  * Released under the MIT license
